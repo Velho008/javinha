fc9d1e4beadb07399ea51bcf792063ff2091ac66
Velho008 <playergueico@gmail.com>
2025-03-12 19:09:15 -0300

    dps trocar anotacoes pra .java

---
+++ javinha/anotações/casting.java
@@ -0,0 +1,16 @@
+public class casting // 4
+{
+    // casting em java é trocar tipos de variaveis:
+
+    // de num com virgula para num inteiro e vice-versa:
+    float dindin = 50.922f; // float 
+    short dindinInt = (short)dindin; // nova variavel agr do tipo short
+    short dinheiro = 50; // short
+    float dineros = (float)dinheiro; // nova variavel do tipo float
+
+
+    // de string pra numero e vice-versa:
+    String Entrada = "50"; // string
+    int numEntrada = Integer.parseInt(Entrada); //nova variavel do tipo int
+    String voltaDaEntrada = String.valueOf(numEntrada); //nova variavel agr do tipo String
+}
