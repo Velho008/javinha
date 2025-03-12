0a78a7a3ea3513f7ccd239ebfa78c4e92e787abb
Velho008 <playergueico@gmail.com>
2025-03-12 13:48:44 -0300

    nada

--- javinha/anotações/dissecando_codigo_simples.py
+++ javinha/anotações/dissecando_codigo_simples.py
@@ -3,7 +3,7 @@
 #import java.util.ArrayList; #permite cria listas
 #
 #public class Test { #public Significa que o método ou variável pode ser acessado de qualquer lugar #class cria a classe "Test"
-#          #static Indica que o método ou variável pertence à classe e não a um objeto da classe #void Significa que o método não retorna nenhum valor 
+#          #static Indica que o método ou variável pertence à classe e não a um objeto da classe #void Significa que o método não retorna nenhum valor.
 #    public static void main(String[] args) {  #main é o ponto de entrada de um programa java #String[] é obrigatorio pra todo main java, args tambem, mas pode mudar de nome
 #        ArrayList<String> nomes = new ArrayList<>(); #cria uma nova arraylist de strings chamada nomes
 #        nomes.add("joao"); #adiciona joao a nomes
@@ -12,9 +12,9 @@
 #
 #        for (int i = 0; i < nomes.size(); i++) { #i começa em 0 e enquanto for menor que o tamanho de nomes (quantos nomes tem), i aumenta em 1 e o codigo é repetido
 #            System.out.println(nomes.get(i)); #printa a posição de i em nomes, nomes.get(0) == "joao"
-#        }#fecha o for
-#    }#fecha o main
-#}#fecha o Test
+#        } #fecha o for
+#    } #fecha o main
+#} #fecha o Test
 
 #o codigo sem anotações é;
 
