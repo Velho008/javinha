514f839f2b89fbf310cc17c422a2a605e02b6ccb
Velho008 <playergueico@gmail.com>
2025-03-18 09:39:46 -0300

    nadica de nada

--- javinha/exercicios_aleatorios/Test.java
+++ javinha/exercicios_aleatorios/Test.java
@@ -1,7 +1,16 @@
 public class Test
 {
+    public static int century(int number)
+    {
+        int coiso =0;
+        if (number%100 >0)
+        {
+            coiso =1;
+        }
+        return (number/100)+coiso;
+    }
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        System.out.println(century(1705));
     }
 }
