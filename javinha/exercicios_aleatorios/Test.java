dd3c4256baae14a0a41157a2aff8119c2bbb4ba7
Velho008 <playergueico@gmail.com>
2025-03-26 07:22:03 -0300

    nada

--- javinha/exercicios_aleatorios/Test.java
+++ javinha/exercicios_aleatorios/Test.java
@@ -1,4 +1,12 @@
 public class Test
 {
+    public static void main(String[] args)
+    {
+    System.out.println("numero      quadrado        cubo");
+    for (int i=0;i<=10;i++)
+    {
+        System.out.println(i+"          "+(i*i)+"               "+(i*i*i));
+    }
+    }
 
 }
