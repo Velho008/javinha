94e9eee507f8c869c4936b87241d3d246afadba4
Velho008 <playergueico@gmail.com>
2025-03-30 11:48:30 -0300

    tarefinha

--- javinha/exercicios_aleatorios/Tarefinha_extra_classe.java
+++ javinha/exercicios_aleatorios/Tarefinha_extra_classe.java
@@ -51,7 +51,64 @@ public class Tarefinha_extra_classe
             System.out.println("Conversão inválida!");
         }
         //5 Quais as instruções de controle e de repetição em Java? Apresente-as junto de suas sintaxes.
-        //tá tudo em loops e em switch, tem if else e else if tmb, mas esses eu n falei pq n precisa
+        //tá tudo em loops e em switch, tem if, else e else if tmb, mas esses eu n falei pq n precisa
+
+        //6 O que é uma referência em Java?
+        // diferente do tipo primitivo que armazena diretamente o valor, a referencia guarda em si o endereço do objeto
+        // ao modificar o objeto, todas as referencias a ele mudam, por todos referenciarem o mesmo objeto]
+
+        //7 Como se declara uma referência para um vetor em Java? Como se declara uma referência para uma matriz em Java?
+        //tipo[] nome; //criou um vetor
+        //tipo[][] nome; //criou uma matriz
+        int[] vetor = {1,2,3};
+        int[][] matriz = {{0,1,2},{3,4,5},{6,7,8}};
+        System.out.println(vetor); //vai printar o endereço na memoria, não o vetor
+        for (int num : vetor) // loop para printar o vetor
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println(); //adicionar uma quebra de linha dps do ultimo valor 
+        for (int i = 0; i<matriz.length;i++) //loop para cada vetor dentro da matriz
+        {
+            for (int j = 0; j< matriz[i].length; j++) //loop para cada valor dento dos vetores
+            {
+                System.out.print(matriz[i][j] + " "); //printa com espaço entre
+            }
+            System.out.println(); //mesma quebra de linha dps do ultimo valor
+        }
+
+        //8 Como se cria (instancia) um vetor e uma matriz em Java? Apresente exemplos de criação de vetor e matriz de inteiros.
+        //ja foi respondido acima
+
+        //9 O que são funções e procedimentos? Como uma função é definida em Java?
+        //função retorna valor, portanto, tem return
+        //procedimento não retorna um valor, portanto, um metodo void
+        // definindo uma função em java:
+        // modificador tipoDeRetorno nome (parametros) // modificador define a segurança, public, private, default ou protected, o tipo é oq retorna (pode ser void), parametros é oq recebe de fora
+        //{
+            //return valor; //no caso de ser uma função
+        //}
+        
+        //10 Apresente o que será impresso pela execução do seguinte algoritmo em Java. (ta no pdf)
+        //6, 18, 30, 42, 54, 66, 78, 90, 102, 114,
+        //o codigo:
+        int[] n;
+        int a = 2, b = 4;
+        n = new int[10]; //passa a ter 10 espaços
+
+        for (int i = 0; i<10; i++)
+        {
+            while (a%3 !=0)
+            {
+                a+=b;
+            }
+            n[i] = a;
+            a+=b;
+        }
+        for (int i = 0; i<10; i++)
+        {
+            System.out.print(n[i] + ", ");
+        }
     }
 }
         
